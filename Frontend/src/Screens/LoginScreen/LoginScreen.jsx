b58d74391bfaccb531778b62b00bfeba99c29e06
Brian-s-dev <brianesosa@gmail.com>
2026-06-02 19:02:47 -0300

    lala

--- Frontend/src/Screens/LoginScreen/LoginScreen.jsx
+++ Frontend/src/Screens/LoginScreen/LoginScreen.jsx
@@ -1,22 +1,39 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router'
+import { Link, useNavigate } from 'react-router'
 import useForm from '../../hooks/useForm'
 import { login } from '../../services/authService'
 
 export const LoginScreen = () => {
+
+    const navigate = useNavigate();
+
+    const [error, setError] = useState(null);
 
     const initial_form_state = {
         email: '',
         password: ''
     }
 
-    function onSubmit(formData) {
-        console.log("un usuario intento iniciar sesion", formData)
-        login(formData.email, formData.password)
+    async function onSubmit(formData) {
+        setError(null);
+
+        try {
+            const data = await login(formData.email, formData.password);
+
+            if (data.ok) {
+
+                console.log("Login exitoso, redirigiendo a la pantalla de bienvenida...");
+
+                navigate('/home');
+            } else {
+                setError(data.message);
+            }
+        } catch (err) {
+            setError(err.message || "Error al intentar iniciar sesión");
+        }
     }
 
     const { formState, handleChange, handleSubmit } = useForm(initial_form_state, onSubmit)
-
 
     return (
         <div>
@@ -25,12 +42,15 @@ export const LoginScreen = () => {
             <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="email">Email:</label>
-                    <input id='email' name='email' type='email' value={formState.email} onChange={handleChange} />
+                    <input id='email' name='email' type='email' value={formState.email} onChange={handleChange} required />
                 </div>
                 <div>
                     <label htmlFor="password">Contraseña:</label>
-                    <input id='password' name='password' type='password' value={formState.password} onChange={handleChange} />
+                    <input id='password' name='password' type='password' value={formState.password} onChange={handleChange} required />
                 </div>
+
+                {/* 6. Renderizamos condicionalmente el mensaje de error si existe */}
+                {error && <p style={{ color: 'red', margin: '10px 0', fontSize: '14px' }}>❌ {error}</p>}
 
                 {/* AQUÍ AGREGAMOS EL ENLACE DE OLVIDÉ MI CONTRASEÑA */}
                 <div style={{ margin: '10px 0' }}>
