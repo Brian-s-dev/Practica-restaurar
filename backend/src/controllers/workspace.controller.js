0b9d73ba01ca6ae1bb477d41680b4b8feebde002
Brian-s-dev <brianesosa@gmail.com>
2026-06-09 15:35:39 -0300

    services

--- backend/src/controllers/workspace.controller.js
+++ backend/src/controllers/workspace.controller.js
@@ -1,7 +1,9 @@
 import ServerError from "../helpers/serverError.helper.js";
 import workspaceService from "../services/workspace.service.js";
+import workspaceRepository from "../repositories/workspace.repository.js";
 
 class WorkspaceController {
+
     async create(request, response) {
         const { nombre, descripcion } = request.body;
         const user_id = request.user.id;
@@ -18,5 +20,41 @@ class WorkspaceController {
             data: { workspace: newWorkspace }
         });
     }
+
+    async getAllByUser(request, response) {
+        const user_id = request.user.id;
+        const workspaces = await workspaceRepository.getAllWorkspacesByUserId(user_id);
+
+        return response.status(200).json({
+            ok: true,
+            message: "Espacios de trabajo obtenidos",
+            data: { workspaces }
+        });
+    }
+
+    async deleteById(request, response) {
+        const { workspace_id } = request.params;
+
+        await workspaceRepository.deleteById(workspace_id);
+
+        return response.status(200).json({
+            ok: true,
+            message: "Espacio de trabajo eliminado"
+        });
+    }
+
+    async updateById(request, response) {
+        const { workspace_id } = request.params;
+        const { nombre, descripcion } = request.body;
+
+        const updatedWorkspace = await workspaceRepository.updateById(workspace_id, { nombre, descripcion });
+
+        return response.status(200).json({
+            ok: true,
+            message: "Espacio de trabajo actualizado",
+            data: { workspace: updatedWorkspace }
+        });
+    }
 }
+
 export default new WorkspaceController();
